c4d0452545ec568f22a8eda7914ab022c0546539
xdpooja <justpooja16@gmail.com>
2023-04-26 14:22:24 +0530

    new changes

---
+++ Amazon.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+
+train = pd.read_csv(r'D:/python/machine learning hackathon/test.csv').dropna()
+test = pd.read_csv(r'D:/python/machine learning hackathon/train.csv').dropna()
+
+print(test.isna().sum())
+x = train.iloc[]
+plt.plot()
